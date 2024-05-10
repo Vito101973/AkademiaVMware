b9f597d714a5cb360f0a9785fcd44cbdcffeeaf6
Vito101973 <88482504+Vito101973@users.noreply.github.com>
2024-05-10 10:27:34 +0200

    Update esxcliPSmix.ps1

--- esxcliPSmix.ps1
+++ esxcliPSmix.ps1
@@ -7,9 +7,13 @@ $esxcli
 $esxcli.system.account.list.Invoke()
 #nowy user z esxcli in PS
 $esxcli.system.account.add.Invoke(@{id='foo' ; password= 'Fujitsu2021!' ; passwordconfirmation= 'Fujitsu2021!'})
+#usuniecie usera esxcli in PS
 $esxcli.system.account.remove.Invoke(@{id='foo'})
+#usuniecie permission list esxcli in PS
 $esxcli.system.permission.list.Invoke()
+#nadanie permission list esxcli in PS
 $esxcli.system.permission.set.Invoke(@{id= 'foo1' ; role= 'ReadOnly'})
+#remove permission list esxcli in PS
 $esxcli.system.permission.unset.Invoke(@{id= 'foo'})
 
 
